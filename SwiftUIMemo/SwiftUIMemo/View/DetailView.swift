9782f998f9de49c90b0872a30ae9d36170cdd4fb
Doogie <chris_97@naver.com>
2023-12-19 15:28:01 +0900

    feat: 삭제 기능 구현
    toolbar -> hstack으로 대체
    #1

--- SwiftUIMemo/SwiftUIMemo/View/DetailView.swift
+++ SwiftUIMemo/SwiftUIMemo/View/DetailView.swift
@@ -11,6 +11,9 @@ struct DetailView: View {
     @ObservedObject var memo: Memo
     
     @State private var showComposer = false
+    @State private var showDeleteAlert = false
+    
+    @Environment(\.dismiss) var dismiss
     
     @EnvironmentObject var store: MemoStore
     
@@ -31,18 +34,41 @@ struct DetailView: View {
                         .foregroundStyle(.secondary)
                 }
             }
-        }
-        .navigationTitle("메모 보기")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .bottomBar) {
+            HStack {
+                Spacer()
+                Button {
+                    showDeleteAlert = true
+                } label: {
+                    Image(systemName: "trash")
+                        .resizable()
+                        .frame(width: 20, height: 23)
+                }
+                .foregroundStyle(.red)
+                .alert("삭제 확인", isPresented: $showDeleteAlert) {
+                    Button(role: .destructive) {
+                        store.delete(memo: memo)
+                        dismiss()
+                    } label: {
+                        Text("삭제")
+                    }
+                    
+                } message: {
+                    Text("메모를 삭제하시겠습니까?")
+                }
+                Spacer(minLength: 290)
                 Button {
                     showComposer = true
                 } label: {
                     Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width: 23, height: 23)
                 }
+                Spacer()
             }
+            Spacer()
         }
+        .navigationTitle("메모 보기")
+        .navigationBarTitleDisplayMode(.inline)
         .sheet(isPresented: $showComposer) {
             ComposeView(memo: memo)
         }
